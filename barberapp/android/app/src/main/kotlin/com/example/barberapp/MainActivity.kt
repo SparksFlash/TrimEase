eef7c38a5c7abf4a9284ee4d117e2a29c6872c7b
SparksFlash <ug2102056@cse.pstu.ac.bd>
2025-12-02 08:32:45 +0600

    deleted

--- barberapp/android/app/src/main/kotlin/com/example/barberapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.barberapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- barberapp/lib/config/cloudinary_config.dart
+++
@@ -1,7 +0,0 @@
-// Cloudinary configuration.
-// Replace the values below with your Cloudinary account settings.
-// Create an unsigned upload preset in your Cloudinary dashboard and put its name
-// into `UPLOAD_PRESET`. Keep your cloud name here.
-
-const String CLOUDINARY_CLOUD_NAME = 'dbsmkli6k';
-const String CLOUDINARY_UPLOAD_PRESET = 'ml_default';
